0984633ad174e93ec62de7d2bc9560832d7a9602
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2023-06-09 15:53:05 +0100

    testing

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ document.querySelector('button')?.addEventListener('click', async () => {
 
 const sampler = new Tone.Sampler({
 	urls: {
-		"C4": "42247__timkahn__c_s-cello-c4.flac",
+		"C4": "cello-short.flac",
 	},
 	release: 1,
 }).toDestination();
@@ -16,7 +16,7 @@ const sampler = new Tone.Sampler({
 function handleQwerty(event) {
     let key = event.key.toUpperCase();
     keys = ["Z", "X", "C", "V"];
-    notes = ["A3", "C4", "E4", "G4"];
+    notes = ["A3", "B3", "C4", "D4"];
 
     for(let i = 0; i < keys.length; i++){
         if (key === keys[i]){
